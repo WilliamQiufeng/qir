9330ee16aebc277e89e948344e9590e0ade1d1e3
WilliamQiufeng <williamqiufeng@outlook.com>
2025-01-20 22:09:29 +0000

    Add graph lib

--- src/main/scala/tac/Block.scala
+++ src/main/scala/tac/Block.scala
@@ -1,7 +1,17 @@
 package tac
 
+import scalax.collection.generic.{AbstractDiEdge, Edge}
+import scalax.collection.mutable.Graph
+
 import scala.collection.mutable
 
+class BlockEdge(source: Block, target: Block) extends AbstractDiEdge[Block](source, target) {
+
+}
 
 class Block(val tacs: mutable.ArrayBuffer[Tac]) {
+}
+
+implicit class ExtGraph(val g: Graph[Block, BlockEdge]) {
+  def foo: String = g.toString
 }
